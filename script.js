6c0bf4dd357ee7ebd2c9b1013a05b0f5d11b899c
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-05-07 08:46:11 -0600

    changed back to Portuguese

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-var broken = false;
-function changeLight(type) {
-	if (type == 1){
-		archive = "light-on.jpg";
+var quebrada = false;
+function mudaLampada(tipo) {
+	if (tipo == 1){
+		arquivo = "lampada-acesa.jpg";
 	}
-	if (type == 2){
-		archive = "light-off.jpg";
+	if (tipo == 2){
+		arquivo = "lampada-apagada.jpg";
 	}
-	if (type == 3){
-		archive = "light-broken.jpg";
+	if (tipo == 3){
+		arquivo = "lampada-quebrada.jpg";
 	}
-	if (!broken){
-		document.getElementById("light").src = archive;
-		if (type == 3){
-			broken = true;
+	if (!quebrada){
+		document.getElementById("luz").src = arquivo;
+		if (tipo == 3){
+			quebrada = true;
 		}
 	}
 }
